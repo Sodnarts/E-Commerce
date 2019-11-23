bdc87e008a4578a909c9b034bf7ee4359795d358
Glenn Robert Strandos <strandos.glenn@gmail.com>
2019-11-23 11:07:49 +0100

    Effectivise data handling

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,7 +7,7 @@ const CollectionPreview = ({ title, items }) => (
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
             {items
-                .filter((item, index) => index < 4)
+                .filter((item, index) => index < 4) // TODO: Extract the length to Config.js file
                 .map((item) => (
                     <CollectionItem key={item.id} item={item} />
                 ))}
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import ShopData from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { Route } from 'react-router-dom';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: ShopData,
-        };
-    }
-
-    render() {
-        const { collections } = this.state;
-
-        return (
-            <div>
-                {collections.map(({ id, ...props }) => (
-                    <CollectionPreview key={id} {...props} />
-                ))}
-            </div>
-        );
-    }
-}
+const ShopPage = ({ match }) => (
+    <div>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+);
 
 export default ShopPage;
